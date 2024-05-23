4f32f11c18d7b0c03d4201625799ac06e31c4bd3
Matheus Braga <matheusgalaxytab@gmail.com>
2024-05-23 19:13:20 -0400

    feat: add DateFormatAttribute class for handling user datetime operations

---
+++ tMovies.API/Utilities/DateFormatAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace tMovies.API.Utilities
+{
+    public class DateFormatAttribute : ValidationAttribute
+    {
+        private readonly string _dateFormat;
+
+        public DateFormatAttribute(string dateFormat)
+        {
+            _dateFormat = dateFormat;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult("Data de nascimento é obrigatória.");
+            }
+
+            var dateString = value.ToString();
+            if (DateTime.TryParseExact(dateString, _dateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out _))
+            {
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult($"A data deve estar nesse formato {_dateFormat}.");
+        }
+    }
+}
