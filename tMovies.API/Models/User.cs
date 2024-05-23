e0d1d1ec666fd9f1782148f0ec631f03182a5209
Matheus Braga <matheusgalaxytab@gmail.com>
2024-05-23 19:08:29 -0400

    model: add 'DateOfBirth' property to user model

--- tMovies.API/Models/User.cs
+++ tMovies.API/Models/User.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace tMovies.API.Models
@@ -6,6 +7,10 @@ namespace tMovies.API.Models
     [Table("users")]
     public class User : IdentityUser
     {
+        [Required]
+        [Column(TypeName = "date")]
+        public DateTime DateOfBirth { get; set; }
+
         public ICollection<Profile> Profiles { get; set; }
     }
 }
