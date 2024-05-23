541052a96c65360f63afb2e435880215ec609827
Matheus Braga <matheusgalaxytab@gmail.com>
2024-05-23 19:11:16 -0400

    model: add 'DateOfBirth' property to UserViewModel

--- tMovies.API/ViewModels/RegisterUserViewModel.cs
+++ tMovies.API/ViewModels/RegisterUserViewModel.cs
@@ -1,9 +1,15 @@
-﻿namespace tMovies.API.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using tMovies.API.Utilities;
+
+namespace tMovies.API.ViewModels
 {
     public class RegisterUserViewModel
     {
         public string UserName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        [DateFormat("dd-MM-yyyy")]
+        public string DateOfBirth { get; set; }
     }
 }
